b1b887b7272754d89d48aa3e2ee706144bf981ca
Igor <igor53c@gmail.com>
2024-03-23 16:30:20 +0100

    Add project files.

---
+++ Allergies/AllergiesCsharpImplementation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AllergiesCsharpImplementation
+{
+    public enum Allergen
+    {
+        Eggs = 1 << 0,
+        Peanuts = 1 << 1,
+        Shellfish = 1 << 2,
+        Strawberries = 1 << 3,
+        Tomatoes = 1 << 4,
+        Chocolate = 1 << 5,
+        Pollen = 1 << 6,
+        Cats = 1 << 7
+    }
+
+    public class Allergies
+    {
+        public readonly int score;
+
+        public static Allergies CreateAllergy(Allergen[] allergens)
+        {
+            // scenario 3
+            int score = allergens.Aggregate(0, (current, allergen) => current | (int)allergen);
+            return new Allergies(score);
+        }
+
+        public Allergies(int score) => this.score = score;
+
+        public bool IsAllergicTo(Allergen allergen)
+        {
+            // scenario 1
+            return (score & (int)allergen) == (int)allergen;
+        }
+
+        public Allergen[] List()
+        {
+            // scenario 2
+            var allergiesList = new List<Allergen>();
+            foreach (Allergen allergen in Enum.GetValues(typeof(Allergen)))
+            {
+                if (IsAllergicTo(allergen))
+                {
+                    allergiesList.Add(allergen);
+                }
+            }
+            return allergiesList.ToArray();
+        }
+    }
+}
---
+++ Allergies/AllergiesTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using AllergiesCsharpImplementation;
+
+[TestFixture]
+public class AllergiesTests
+{
+    [Test]
+    public void IsAllergicTo_WithScoreZero_ShouldReturnFalseForAnyAllergen()
+    {
+        var allergies = new AllergiesCsharpImplementation.Allergies(0);
+        Assert.That(allergies.IsAllergicTo(Allergen.Eggs), Is.False, "Expected no allergy to Eggs, but was found.");
+        Assert.That(allergies.IsAllergicTo(Allergen.Peanuts), Is.False, "Expected no allergy to Peanuts, but was found.");
+        // You can add assertions for other allergens as needed
+    }
+
+    [Test]
+    public void IsAllergicTo_WithScoreOne_ShouldReturnTrueForEggs()
+    {
+        var allergies = new AllergiesCsharpImplementation.Allergies(1);
+        Assert.That(allergies.IsAllergicTo(Allergen.Eggs), Is.True, "Expected an allergy to Eggs, but wasn't found.");
+    }
+
+    [Test]
+    public void IsAllergicTo_WithScoreSeventeen_ShouldReturnTrueForTomatoes()
+    {
+        var allergies = new AllergiesCsharpImplementation.Allergies(17);
+        Assert.That(allergies.IsAllergicTo(Allergen.Tomatoes), Is.True, "Expected an allergy to Tomatoes, but wasn't found.");
+    }
+
+    [Test]
+    public void List_WithScoreZero_ShouldReturnEmptyArray()
+    {
+        var allergies = new AllergiesCsharpImplementation.Allergies(0);
+        Assert.That(allergies.List(), Is.Empty, "Expected no allergies, but some were found.");
+    }
+
+    [Test]
+    public void List_WithScoreOne_ShouldReturnEggs()
+    {
+        var allergies = new AllergiesCsharpImplementation.Allergies(1);
+        Assert.That(allergies.List(), Is.EquivalentTo(new[] { Allergen.Eggs }), "Expected a list containing only Eggs.");
+    }
+
+    [Test]
+    public void CreateAllergy_WithEmptyArray_ShouldReturnScoreZero()
+    {
+        var allergies = AllergiesCsharpImplementation.Allergies.CreateAllergy(new Allergen[] { });
+        Assert.That(allergies.score, Is.EqualTo(0), "Expected score to be 0 for an empty allergen list.");
+    }
+
+    [Test]
+    public void CreateAllergy_WithEggsArray_ShouldReturnScoreOne()
+    {
+        var allergies = AllergiesCsharpImplementation.Allergies.CreateAllergy(new[] { Allergen.Eggs });
+        Assert.That(allergies.score, Is.EqualTo(1), "Expected score to be 1 for an allergen list containing only Eggs.");
+    }
+}
